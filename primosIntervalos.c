eba3253e45030078289bc37a2f26541ddb814ffe
difierro <gustavo7867grdr@gmail.com>
2023-09-27 15:07:51 -0300

    correções de bugs

--- primosIntervalos.c
+++ primosIntervalos.c
@@ -1,8 +1,3 @@
-/*
- *erro: Exceção de ponto flutuante (imagem do núcleo gravada)
- *na função calcPrimos falta arrumar o return, pois esta dando como void
- */
-
 #include <stdio.h>
 
 int calcPrimos();
@@ -15,8 +10,8 @@ int main(){
     scanf("%d", &num2);
 
     for(int num = num1; num < num2; num++){
-        if (calcPrimos(num) == 1){
-            printf("%d", num);
+        if (calcPrimos(num) == 1){  // se for verdadeiro, ele é primo
+            printf("%d \n", num);
         }
         
     }
@@ -25,14 +20,20 @@ int main(){
 }
 
 int calcPrimos(int num){
-    
-    for(int i = 0; i < num ;i++){
+    if (num <= 1){
+        return 0;   //o 1 e o 0 não são primos, então retornam falso 
+    }
+
+    if( (num % 2== 0) && (num > 2)){
+        return 0;   // os numeros pares maiores que 2 não são primos
+    }
+
+    for(int i = 3; i < num ;i += 2){
         if ( num % i == 0){
-            return 1;
-        }else{
-            return 0;
+            return 0;   //se o número foi divisível por i (qualquer valor que não seja 1 e o int num), ele não é primo
         }
     }
-    
+
+     return 1;
 }
 
